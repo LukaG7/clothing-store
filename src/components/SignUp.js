3598e04bef90c8fbdf65643ee43527ffe2b8c1e1
Lukas Gonzalez <lukegonz47@Lukass-MacBook-Pro.local>
2022-11-10 19:04:06 -0500

    added signup form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,7 +3,25 @@
 function SignUp() {
     return (
         <div>
+            
             <h1>Create An Account</h1>
+
+            <div className="signup-div">
+
+                <form className="signup-form">
+
+                    <label className="label">Email</label>
+                    <input className="input"></input>
+                    <label className="label">Password</label>
+                    <input className="input"></input>
+                    <label className="label">Confirm Password</label>
+                    <input className="input"></input>
+                    <button className="signup-btn">Submit</button>
+
+                </form>
+
+            </div>
+
         </div>
     )
 }
